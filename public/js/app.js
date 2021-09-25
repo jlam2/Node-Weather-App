407ee3170d9d50e231560befb8a723698ba71d7c
Jason Lam <32461232+jlam2@users.noreply.github.com>
2021-09-25 01:52:59 +0000

    Added search box and output to homepage

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
-console.log('Client side javascript file is loaded!')
+
+const message = document.querySelector('#message')
+
+const searchInput = document.querySelector('input')
+const searchForm = document.querySelector('form')
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    locationUrl = `/weather?address=${searchInput.value}` 
+    message.textContent = 'Fetching.....'
+    fetch(locationUrl).then((res) => {
+        res.json().then((data) => {
+            if(data.error){
+                message.textContent = data.error
+            }else{
+                message.innerHTML = `Location: ${data.location}<br>Forecast: ${data.forecast}`
+            }
+        })
+    })
+    console.log('form submitted')
+})
